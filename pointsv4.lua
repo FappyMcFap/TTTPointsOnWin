13584e1a4f4cd966a9304a94f27917e6f4e7764d
FappyMcFap <mwkidd97@gmail.com>
2017-01-06 03:42:53 -0500

    Add files via upload

---
+++ pointsv4.lua
@@ -0,0 +1,41 @@
+local function TTTWinRoundPoints(result)
+--	if result == WIN_TIMELIMIT then return end 
+	if result == WIN_INNOCENT then 
+		for _,ply in pairs(player.GetAll() ) do
+			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
+				if ply:Alive() then
+					ply:PS_GivePoints( 35 )
+					ply:ChatPrint("You've been given 35 points for winning and surviving the round as an Innocent!")
+				elseif not ply:Alive() then 
+					ply:PS_GivePoints( 15 )
+					ply:ChatPrint("Unfortunately, you're dead. You do however get 15 points since your Innocent team has won!")
+				end
+			end
+		end
+	elseif result == WIN_TRAITOR then
+		for _,ply in pairs(player.GetAll() ) do
+			if ply:GetRole() == ROLE_TRAITOR then
+				if ply:Alive() then
+					ply:PS_GivePoints( 70 )
+					ply:ChatPrint("You've been given 70 points for winning and surviving the round a Traitor!")
+				elseif not ply:Alive() then
+					ply:PS_GivePoints( 20 ) 	
+					ply:ChatPrint("Unfortunately, you're dead. You do however get 20 points since your Traitor team has won!")
+				end
+			end
+		end
+	elseif result == WIN_TIMELIMIT then 
+		for _,ply in pairs(player.GetAll() ) do
+			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
+				if ply:Alive() then
+					ply:PS_GivePoints( 50 )
+					ply:ChatPrint("The time has run out! What are you doing Traitors?! You're awarded 50 points for the wait.")
+				elseif not ply:Alive() then 
+					ply:PS_GivePoints( 25 )
+					ply:ChatPrint("You're dead and you had to wait for the Traitors to do their jobs, here's 25 points for your troubles!")
+				end
+			end
+		end
+	end
+end -- end func
+hook.Add("TTTEndRound", "WinPoints", TTTWinRoundPoints)
